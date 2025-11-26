5b622d8ca4c73f7d2434ec958ab57f664cab0c25
orrans <orrans@gmail.com>
2025-11-26 22:44:59 +0200

    Add todo logic

--- apps/note/cmps/CreateNoteForm.jsx
+++ apps/note/cmps/CreateNoteForm.jsx
@@ -1,23 +1,91 @@
 import { noteService } from '../services/note.service.js'
 
-const { useState, useEffect } = React
+const { useState, useEffect, useRef } = React
 
 export function CreateNoteForm({ onCreate }) {
     const [note, setNote] = useState(noteService.getEmptyNote())
+    const formRef = useRef(null)
 
-    return (
-        <div
-            onBlur={() => {
-                if (!note.info.txt) return
+    useEffect(() => {
+        function handleClickOutside(event) {
+            const formContainer = formRef.current
+            const clickedElement = event.target
+            // check that the click element is not inside the form container
+            if (formContainer && !formContainer.contains(clickedElement)) {
+                if (note.type === 'NoteTxt' && !note.info.txt) return
+                // check that at least one todo have text
+                if (note.type === 'NoteTodos' && !note.info.todos.some((todo) => todo.txt)) return
+
                 onCreate({ ...note, createdAt: Date.now() })
                 setNote(noteService.getEmptyNote())
-            }}>
-            <input
-                type="text"
-                value={note.info.txt}
-                placeholder="Take a note..."
-                onInput={(event) => setNote({ ...note, info: { txt: event.target.value } })}
-            />
+            }
+        }
+
+        //the listener is on all the body
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => document.removeEventListener('mousedown', handleClickOutside)
+    }, [note])
+
+    function updateTodo(idx, todo) {
+        const todos = [...note.info.todos]
+        todos[idx] = todo
+        setNote({ ...note, info: { todos } })
+    }
+
+    return (
+        <div className="create-form" ref={formRef}>
+            <div className="input-row">
+                {note.type === 'NoteTxt' && (
+                    <input
+                        type="text"
+                        value={note.info.txt}
+                        placeholder="Take a note..."
+                        onInput={(event) => setNote({ ...note, info: { txt: event.target.value } })}
+                    />
+                )}
+                {note.type === 'NoteTodos' && (
+                    <div>
+                        {note.info.todos.map((todo, idx) => (
+                            <div key={idx}>
+                                <input
+                                    type="checkbox"
+                                    checked={todo.isDone}
+                                    onChange={(event) =>
+                                        updateTodo(idx, { ...todo, isDone: event.target.checked })
+                                    }
+                                />
+                                <input
+                                    className={todo.isDone ? 'checked' : ''}
+                                    type="text"
+                                    value={todo.txt}
+                                    onInput={(event) =>
+                                        updateTodo(idx, { ...todo, txt: event.target.value })
+                                    }
+                                />
+                            </div>
+                        ))}
+                        <div
+                            onClick={() =>
+                                updateTodo(note.info.todos.length, { txt: '', isDone: false })
+                            }>
+                            + List item
+                        </div>
+                    </div>
+                )}
+                <button
+                    onClick={() => {
+                        setNote({
+                            ...note,
+                            type: 'NoteTodos',
+                            info: { todos: [{ txt: '', isDone: false }] },
+                        })
+                    }}>
+                    <i className="fa-regular fa-square-check"></i>
+                </button>
+                <button>
+                    <i className="fa-regular fa-image"></i>
+                </button>
+            </div>
         </div>
     )
 }
--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -12,11 +12,26 @@ export function NoteIndex() {
     }, [])
 
     function loadNotes() {
-        noteService.query().then((notes) => setNotes(notes.sort((a, b) => b.createdAt - a.createdAt)))
+        noteService
+            .query()
+            .then((notes) => setNotes(notes.sort((a, b) => b.createdAt - a.createdAt)))
     }
 
     function onCreateNote(note) {
         noteService.save(note).then((createdNote) => setNotes([createdNote, ...notes]))
+    }
+
+    function updateTodo(note, idx, todo) {
+        const todos = [...note.info.todos]
+        todos[idx] = todo
+        onUpdateNote({ ...note, info: { todos } })
+    }
+
+    function onUpdateNote(note) {
+        const updatedNotes = [...notes]
+        const idx = updatedNotes.findIndex((currNote) => currNote.id === note.id)
+        updatedNotes[idx] = note
+        noteService.save(note).then(() => setNotes(updatedNotes))
     }
 
     function onRemoveNote(noteId) {
@@ -36,7 +51,7 @@ export function NoteIndex() {
     return (
         <section className="note-index">
             <CreateNoteForm onCreate={onCreateNote} />
-            <NoteList notes={notes} onRemove={onRemoveNote} />
+            <NoteList notes={notes} onRemove={onRemoveNote} onUpdateTodo={updateTodo} />
         </section>
     )
 }
